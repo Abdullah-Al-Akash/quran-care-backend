4f5d58965e52210922b845b17bbd11ba1fc96b3b
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-04-29 16:46:46 +0600

    Added New Course + Load All Course + Load Single Course Via ID

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config;
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +8,45 @@ const port = process.env.PORT || 5000;
 // Use Middleware:
 app.use(cors());
 app.use(express.json());
+
+// MongoDb Connection:
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ph-5.b3v7f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+        try {
+                await client.connect()
+                const courseCollection = client.db("Quran-Care").collection("services");
+
+                // Get All Courses:
+                app.get('/courses', async (req, res) => {
+                        const query = {};
+                        const findAllCourse = courseCollection.find(query);
+                        const allCourse = await findAllCourse.toArray();
+                        res.send(allCourse);
+                })
+
+                // Get Single Course:
+                app.get('/course/:id', async (req, res) => {
+                        const id = req.params.id;
+                        const query = { id: +id };
+                        const course = await courseCollection.findOne(query);
+                        res.send(course);
+                })
+
+                // Add New Course:
+                app.post('/courses', async (req, res) => {
+                        const newCourse = req.body;
+                        const course = await courseCollection.insertOne(newCourse);
+                        res.send(course);
+                })
+        }
+        finally {
+
+        }
+}
+run().catch(console.dir);
 
 // Get From Root Path:
 app.get('/', function (req, res) {
